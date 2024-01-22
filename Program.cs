81585a32526bff475cfea76bdc6e08211ef75fa0
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-22 14:36:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Nomonde";
+            int age;
+
+            name = "Nomonde";
+            age = 15;
+
+            Console.WriteLine(" There was once a girl called " +  name);
+            Console.WriteLine(name  + " was " +  age +  " years of age ");
+            Console.WriteLine(name  +  " really never liked her name ");
+
+            age = 19;
+            name = "Monde";
+
+            Console.WriteLine("At age  " +  age  + " she changed her name to " +  name);
+            Console.ReadLine();
+        }
+    }
+}
